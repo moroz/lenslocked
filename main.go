b3bae4e58a851107b09ed6f736ba3f150f1eaa49
Karol Moroz <karol@moroz.dev>
2023-03-05 13:37:45 +0100

    Implement router as struct

--- main.go
+++ main.go
@@ -5,17 +5,9 @@ import (
 	"net/http"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>你好，世界！</h1>")
-}
+type Router struct{}
 
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>聯絡我</h1>")
-}
-
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
@@ -26,8 +18,18 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>你好，世界！</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>聯絡我</h1>")
+}
+
 func main() {
-	http.HandleFunc("/", pathHandler)
+	var router Router
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 }
