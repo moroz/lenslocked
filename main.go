aba119f0eb654d8e75f0fc26e172cefda63c38c4
Karol Moroz <karol@moroz.dev>
2023-03-07 01:06:03 +0100

    Generate home page using template

--- main.go
+++ main.go
@@ -2,21 +2,26 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func bioHandler(w http.ResponseWriter, r *http.Request) {
-	bio := `<script>alert("Haxored");</script>`
-	w.Header().Set("content-type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>About me</h1>\n<p>"+bio+"</p>")
-}
-
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>你好，世界！</h1>")
+	tpl, err := template.ParseFiles("templates/home.gohtml")
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
@@ -45,7 +50,6 @@ func main() {
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
-	r.Get("/bio", bioHandler)
 	r.Group(func(r chi.Router) {
 		r.Use(middleware.Logger)
 		r.Get("/hello/{name}", helloWorldHandler)
