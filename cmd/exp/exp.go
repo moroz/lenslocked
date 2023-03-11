c9ee1358e936213bbc32e57f0304d1922141c942
Karol Moroz <karol@moroz.dev>
2023-03-11 02:56:11 +0100

    Use DATABASE_URL variable

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -3,12 +3,22 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
+var DBUrl string
+
+func init() {
+	DBUrl = os.Getenv("DATABASE_URL")
+	if DBUrl == "" {
+		panic("Environment variable DATABASE_URL is not set!")
+	}
+}
+
 func main() {
-	db, err := sql.Open("pgx", "postgres://localhost/test")
+	db, err := sql.Open("pgx", DBUrl)
 	if err != nil {
 		panic(err)
 	}
