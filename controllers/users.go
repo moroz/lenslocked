e77e5eb3dd5eb78549deac907c82bc5f9040abe6
Karol Moroz <karol@moroz.dev>
2023-03-11 02:14:58 +0100

    Parse POST form

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -12,4 +13,14 @@ type Users struct {
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
 	u.Templates.New.Execute(w, nil)
+}
+
+func (u Users) Create(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Unable to parse form submission.", http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(w, "<p>Email: %s</p>", r.PostFormValue("email"))
+	fmt.Fprintf(w, "<p>Password: %s</p>", r.PostFormValue("password"))
 }
