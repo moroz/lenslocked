0a1809935f1406b88a66373cc096ce663514a071
Karol Moroz <karol@moroz.dev>
2023-03-11 18:47:45 +0100

    WIP on querying multiple rows

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -32,6 +32,13 @@ create table if not exists orders (
 );
 `
 
+type Order struct {
+	ID          int
+	Amount      int
+	Description string
+	UserID      int
+}
+
 func main() {
 	db, err := sql.Open("pgx", DBUrl)
 	if err != nil {
@@ -45,13 +52,51 @@ func main() {
 	}
 	fmt.Println("Tables created.")
 
-	name := "Example User"
-	email := "user@example.com"
-	row := db.QueryRow(`insert into users (name, email) values ($1, $2) returning id`, name, email)
-	var id int
-	err = row.Scan(&id)
+	// name := "Example User"
+	// email := "user@example.com"
+	// row := db.QueryRow(`insert into users (name, email) values ($1, $2) returning id`, name, email)
+	// var id int
+	// err = row.Scan(&id)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("User created. id =", id)
+
+	// 	id := 1
+	// 	row := db.QueryRow(`select name, email from users where id = $1;`, id)
+	// 	var name, email string
+	// 	err = row.Scan(&name, &email)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	fmt.Printf("User information: name=%s, email=%s\n", name, email)
+
+	// 	for i := 1; i <= 5; i++ {
+	// 		amount := i * 100
+	// 		desc := fmt.Sprintf("Fake order #%d", i)
+	// 		_, err = db.Exec(`
+	// 		insert into orders(user_id, amount, description)
+	// 		values ($1, $2, $3)
+	// 		`, 2, amount, desc)
+	// 		if err != nil {
+	// 			panic(err)
+	// 		}
+	// 	}
+
+	// 	fmt.Println("Created fake orders.")
+
+	rows, err := db.Query(`select name, email from users where id = $1`, 2)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("User created. id =", id)
+	defer rows.Close()
+
+	for rows.Next() {
+		var order Order
+		order.UserID = 2
+		err := rows.Scan(&order.ID, &order.Amount, &order.Description)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
